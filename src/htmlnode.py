a71d4bd837c5d3adec5d24d2c8d8c04b504c5037
Andras Verbenyi <headhunter.germany@gmail.com>
2026-01-28 12:34:39 +0100

    First iteration LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,17 @@ class HTMLNode:
         else:
             return None
     
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+    def __eq__(self, other):
+        if isinstance(other, HTMLNode) == False:
+            return False
+        else:
+            if self.tag == other.tag and self.value == other.value and self.props == other.props:
+                return True
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        super().__init__(tag, value, children, props)        
