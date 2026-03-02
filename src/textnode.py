0b2205b8c41b47824b35a8526f43ce51dd8df173
Andras Verbenyi <headhunter.germany@gmail.com>
2026-03-02 14:36:34 +0100

    Working on textnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     PLAIN = "simple text"
@@ -33,5 +35,8 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+
 
         
